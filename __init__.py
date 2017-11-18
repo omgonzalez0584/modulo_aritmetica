15bd346565df45a2ff31eba7e38baf307ae7652d
Omar Gonzalez <om.gonzalez007@gmail.com>
2017-11-18 10:49:25 -0500

    funcion numero primo
    Se agrega la funcion Numero primo
    Dado un numero te indica si es primo o no

--- __init__.py
+++ __init__.py
@@ -4,13 +4,21 @@ def main():
     print("Programa que realiza operaciones aritmeticas")
     a = int(input("Introduzca el primer numero: "))
     b = int(input("Introduzca el segundo numero: "))
+
     c = aritmetica.suma(a,b)
     print("La suma es: ",c)
+
     c = aritmetica.resta(a,b)
     print("La resta es: ",c)
+
     c = aritmetica.producto(a,b)
     print("El prodcuto es: ",c)
+
     c = aritmetica.division(a,b)
     print("La disivion es: ",c)
+
+    d = int(input("Introduza un numero para validar si es primo: "))
+    aritmetica.numero_primo(d)
+
 main()
 
--- aritmetica.py
+++ aritmetica.py
@@ -10,4 +10,17 @@ def producto(a,b):
 def division(a,b):
     return a/b
 
+#Determina si es un numero primo
+def numero_primo(a):
+    cont = 0
+    for i in range(1,a+1):
+        mod = a % i
+        if mod == 0:
+            cont= cont + 1
+    if cont == 2:
+         print(a,"Es primo")
+    else:
+        print(a,"No es primo")
+
+
 
