ba26e1119d31ead1c75540780920d6c78756f3f7
Omar Gonzalez <om.gonzalez007@gmail.com>
2017-11-18 10:13:25 -0500

    Modulo aritmetica.py
    Este modulo realiza operaciones basicas suma, resta, multiplicacion y division.
    Se estaran agregando mas funciones arimeticas a este modulo.

---
+++ __init__.py
@@ -0,0 +1,16 @@
+import aritmetica
+
+def main():
+    print("Programa que realiza operaciones aritmeticas")
+    a = int(input("Introduzca el primer numero: "))
+    b = int(input("Introduzca el segundo numero: "))
+    c = aritmetica.suma(a,b)
+    print("La suma es: ",c)
+    c = aritmetica.resta(a,b)
+    print("La resta es: ",c)
+    c = aritmetica.producto(a,b)
+    print("El prodcuto es: ",c)
+    c = aritmetica.division(a,b)
+    print("La disivion es: ",c)
+main()
+
